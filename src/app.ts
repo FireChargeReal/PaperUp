8991eaed03bd362825c61e8873f3e2c4f7e729fb
FireChargeReal <48412324+FireChargeReal@users.noreply.github.com>
2021-06-24 23:25:10 +0200

    fix

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@
  * Licensed under the MIT License.
  */
 import * as MRE from "@microsoft/mixed-reality-extension-sdk";
-// import { DialogResponse } from "@microsoft/mixed-reality-extension-sdk/built/internal/payloads";
-// import { MreArgumentError } from "@microsoft/mixed-reality-extension-sdk";
 /**
  * The main class of this app. All the logic goes here.
  */
